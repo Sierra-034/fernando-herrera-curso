102768bd093f740123cb9584fa8fe5bfb2562e7b
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-07-20 18:11:08 -0600

    23: Promesas

---
+++ promesas.js
@@ -0,0 +1,50 @@
+const empleados = [
+    {
+        id: 1,
+        nombre: 'Samuel',
+    },
+    {
+        id: 2,
+        nombre: 'Stephanie',
+    },
+    {
+        id: 3,
+        nombre: 'Mireya',
+    },
+];
+
+const salarios = [
+    {
+        id: 1,
+        salario: 1000,
+    },
+    {
+        id: 2,
+        salario: 1500,
+    },
+];
+
+const getEmpleado = (id) => {
+    return new Promise((resolve, reject) => {
+        const empleado = empleados.find(e => e.id === id)?.nombre;
+        if (empleado) resolve(empleado);
+        else reject(`No existe empleado con id ${id}`);
+    });
+};
+
+const getSalario = (id) => {
+    return new Promise((resolve, reject) => {
+        const salario = salarios.find(s => s.id === id)?.salario;
+        if (salario) resolve(salario);
+        else reject(`No existe salario para el id ${id}`);
+    });
+};
+
+const id = 2;
+getEmpleado(id)
+    .then(console.log)
+    .catch(console.error);
+
+getSalario(id)
+    .then(console.log)
+    .catch(console.error);
